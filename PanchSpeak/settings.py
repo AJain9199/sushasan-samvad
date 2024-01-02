71401636846aaa0cdba96096d5e3920687017c70
Aryaman Jain <aryamanjain9199@gmail.com>
2024-01-02 17:47:21 +0530

    Migrated to SQLite

--- PanchSpeak/settings.py
+++ PanchSpeak/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'PanchSpeak.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
